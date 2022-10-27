470bb35557c9151ca6e7fa66546e588b277efd3f
janatimed18 <janatimed18@gmail.com>
2022-10-27 12:47:05 +0100

    first commit

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+    public static String question[] = {
+            "Which company owns the android",
+            "Which one is not the programming langague?",
+            "Where you are watching this video?"
+
+    };
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsup","Instagram","Youtube"}
+
+    };
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube"
+
+    };
+}
